be8f6cc71197b7e6da86c14c4177a9d1b1287a89
Stephen Cresswell <git@stephen-cresswell.net>
2018-01-02 00:45:46 +0000

    Fix kubernetes-cli

--- server/lib/components/kubernetes/kubernetes-cli.js
+++ server/lib/components/kubernetes/kubernetes-cli.js
@@ -4,8 +4,9 @@ export default function(options = {}) {
 
   function start(cb) {
 
-    function apply(context, manifest, namespace, logger) {
+    function apply(context, namespace, manifest, logger) {
       return new Promise((resolve, reject) => {
+        console.log(['--context', context, '--namespace', namespace, 'apply', '--filename', '-',])
         const kubectl = spawn('kubectl', ['--context', context, '--namespace', namespace, 'apply', '--filename', '-',]);
         kubectl.stdout.on('data', data => {
           logger.info(data.toString().trim());
