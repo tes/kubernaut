cae6b0e88a70eb7e98a4676068106430e2072b3f
Daniel Malon <daniel.malon@me.com>
2018-02-21 10:28:40 +0000

    Bump Elastic APM

--- server/lib/preload.js
+++ server/lib/preload.js
@@ -1,6 +1,6 @@
 import fs from 'fs';
 import path from 'path';
 
-if (fs.existsSync(path.join(process.cwd(), 'elastic-apm-node.js')) || process.env.ELASTIC_APM_CONFIG_FILE || process.env.ELASTIC_APM_APP_NAME) {
+if (fs.existsSync(path.join(process.cwd(), 'elastic-apm-node.js')) || process.env.ELASTIC_APM_CONFIG_FILE || process.env.ELASTIC_APM_SERVICE_NAME) {
   require('elastic-apm-node/start');
 }
