92b0f8555981a3db0c5d9106cdb3ae63a93ae5f7
Stephen Cresswell <git@stephen-cresswell.net>
2018-05-14 10:45:08 +0100

    Minor refactor

--- server/test/test-system.js
+++ server/test/test-system.js
@@ -3,9 +3,7 @@ import { fake as clock } from 'groundhog-day';
 import memorySession from '../lib/components/express/session-memory';
 import kubernetes from '../lib/components/kubernetes/kubernetes-stub';
 
-export default function() {
-  return system()
-      .set('clock', clock())
-      .set('session', memorySession()).dependsOn('config', 'logger')
-      .set('kubernetes', kubernetes()).dependsOn('config', 'logger');
-}
+export default () => system()
+  .set('clock', clock())
+  .set('session', memorySession()).dependsOn('config', 'logger')
+  .set('kubernetes', kubernetes()).dependsOn('config', 'logger');
