6b21b2207827205e8054f9b5c1e9ab9ad09dca99
Chris Flook <chris.flook@tesglobal.com>
2018-07-13 15:45:46 +0100

    Missed removing a thunk

--- client/src/modules/accounts.js
+++ client/src/modules/accounts.js
@@ -5,27 +5,6 @@ export const FETCH_ACCOUNTS_REQUEST = createAction(`${actionsPrefix}/FETCH_ACCOU
 export const FETCH_ACCOUNTS_SUCCESS = createAction(`${actionsPrefix}/FETCH_ACCOUNTS_SUCCESS`);
 export const FETCH_ACCOUNTS_ERROR = createAction(`${actionsPrefix}/FETCH_ACCOUNTS_ERROR`);
 
-export function fetchAccounts(options = { page: 1, limit: 50, quiet: false }) {
-  return async (dispatch) => {
-    const limit = options.limit;
-    const offset = (options.page - 1) * options.limit;
-    let data = { limit, offset, count: 0, items: [] };
-    dispatch(FETCH_ACCOUNTS_REQUEST({ data, loading: true }));
-
-    try {
-      const url = `/api/accounts?limit=${limit}&offset=${offset}`;
-      const res = await fetch(url, { method: 'GET', timeout: options.timeout, credentials: 'same-origin' });
-      if (res.status >= 400) throw new Error(`${url} returned ${res.status} ${res.statusText}`);
-      data = await res.json();
-    } catch(error) {
-      if (options.quiet !== true) console.error(error); // eslint-disable-line no-console
-      return dispatch(FETCH_ACCOUNTS_ERROR({ data, error }));
-    }
-
-    return dispatch(FETCH_ACCOUNTS_SUCCESS({ data }));
-  };
-}
-
 const defaultState = {
   data: {
     limit: 0,
