5f29a6dc8849fa0ee7bd3810f2c6663abf449716
Chris Flook <chris.flook@tesglobal.com>
2018-08-07 12:03:46 +0100

    Add findServices store method

--- server/lib/components/store/service.js
+++ server/lib/components/store/service.js
@@ -1,8 +1,10 @@
 import sqb from 'sqb';
+import Service from '../../domain/Service';
+import Registry from '../../domain/Registry';
 
 export default function(options) {
 
-  const { Op } = sqb;
+  const { Op, raw } = sqb;
 
   function start({ config, logger, db }, cb) {
 
@@ -20,7 +22,65 @@ export default function(options) {
       return result.rows;
     }
 
+    async function findServices(criteria = {}, limit = 50, offset = 0) {
+      logger.debug(`Listing up to ${limit} services starting from offset: ${offset}`);
+
+      const bindVariables = {};
+
+      const findServicesBuilder = sqb
+        .select('s.id', 's.name', 's.created_on', 's.created_by', 'sr.id registry_id', 'sr.name registry_name')
+        .from('active_service__vw s', 'active_registry__vw sr')
+        .where(Op.eq('s.registry', raw('sr.id')))
+        .orderBy('s.name asc')
+        .limit(limit)
+        .offset(offset);
+
+      const countServicesBuilder = sqb
+        .select(raw('count(*) count'))
+        .from('active_service__vw s', 'active_registry__vw sr')
+        .where(Op.eq('s.registry', raw('sr.id')));
+
+      if (criteria.registries) {
+        db.buildWhereClause('sr.id', criteria.registries, bindVariables, findServicesBuilder, countServicesBuilder);
+      }
+
+      const findStatement = db.serialize(findServicesBuilder, bindVariables);
+      const countStatement = db.serialize(countServicesBuilder, bindVariables);
+
+      return db.withTransaction(async connection => {
+        return Promise.all([
+          connection.query(findStatement.sql, findStatement.values),
+          connection.query(countStatement.sql, countStatement.values),
+        ]).then(([findResult, countResult]) => {
+          const items = findResult.rows.map(toService);
+          const count = parseInt(countResult.rows[0].count, 10);
+          logger.debug(`Returning ${items.length} of ${count} services`);
+
+          return {
+            limit,
+            offset,
+            count,
+            items,
+          };
+        });
+      });
+    }
+
+    function toService(row) {
+      return new Service({
+        id: row.id,
+        name: row.name,
+        createdOn: row.createdOn,
+        createdBy: row.createdBy,
+        registry: new Registry({
+          id: row.registry_id,
+          name: row.registry_name,
+        }),
+      });
+    }
+
     return cb(null, {
+      findServices,
       searchByServiceName,
     });
   }
--- server/test/store/service.test.js
+++ server/test/store/service.test.js
@@ -2,7 +2,7 @@ import expect from 'expect';
 import createSystem from '../test-system';
 import { makeRegistry, makeRelease, makeRootMeta } from '../factories';
 
-describe('Service store', () => {
+describe.only('Service store', () => {
   let system = { stop: cb => cb() };
   let store = { nuke: () => new Promise(cb => cb()) };
 
@@ -33,6 +33,75 @@ describe('Service store', () => {
     expect(results[0].name).toBe('app-1');
     expect(results[1].name).toBeDefined();
     expect(results[1].name).toBe('app-2');
+  });
+
+  describe('findServices', () => {
+    it('should find all services', async () => {
+      const registry = await saveRegistry(makeRegistry(), makeRootMeta());
+      const registry2 = await saveRegistry(makeRegistry(), makeRootMeta());
+
+      await saveRelease(makeRelease({ service: { name: 'app-1', registry } }), makeRootMeta());
+      await saveRelease(makeRelease({ service: { name: 'app-2', registry2 } }), makeRootMeta());
+      await saveRelease(makeRelease({ service: { name: 'service-1', registry } }), makeRootMeta());
+
+      const results = await store.findServices();
+      expect(results).toBeDefined();
+      expect(results).toMatchObject({
+        offset: 0,
+        count: 3,
+      });
+      expect(results.items[0].name).toBe('app-1');
+      expect(results.items[1].name).toBe('app-2');
+      expect(results.items[2].name).toBe('service-1');
+    });
+
+    it('should find only services for a specific registry', async () => {
+      const registry = await saveRegistry(makeRegistry(), makeRootMeta());
+      const registry2 = await saveRegistry(makeRegistry(), makeRootMeta());
+
+      await saveRelease(makeRelease({ service: { name: 'app-1', registry } }), makeRootMeta());
+      await saveRelease(makeRelease({ service: { name: 'app-2', registry2 } }), makeRootMeta());
+      await saveRelease(makeRelease({ service: { name: 'service-1', registry } }), makeRootMeta());
+
+      const results = await store.findServices({
+        registries: [registry.id],
+      });
+
+      expect(results).toBeDefined();
+      expect(results).toMatchObject({
+        offset: 0,
+        count: 2,
+      });
+      expect(results.items[0].name).toBe('app-1');
+      expect(results.items[1].name).toBe('service-1');
+    });
+
+    it('should limit and offset', async () => {
+      const registry = await saveRegistry(makeRegistry(), makeRootMeta());
+      const registry2 = await saveRegistry(makeRegistry(), makeRootMeta());
+
+      await saveRelease(makeRelease({ service: { name: 'app-1', registry } }), makeRootMeta());
+      await saveRelease(makeRelease({ service: { name: 'app-2', registry2 } }), makeRootMeta());
+      await saveRelease(makeRelease({ service: { name: 'service-1', registry } }), makeRootMeta());
+
+      const results = await store.findServices({}, 1);
+      expect(results).toBeDefined();
+      expect(results).toMatchObject({
+        offset: 0,
+        count: 3,
+      });
+      expect(results.items.length).toBe(1);
+      expect(results.items[0].name).toBe('app-1');
+
+      const results2 = await store.findServices({}, 1, 1);
+      expect(results2).toBeDefined();
+      expect(results2).toMatchObject({
+        offset: 1,
+        count: 3,
+      });
+      expect(results2.items.length).toBe(1);
+      expect(results2.items[0].name).toBe('app-2');
+    });
   });
 
 
