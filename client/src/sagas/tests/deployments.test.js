71a64360a5364aa409a86167ff6bab695e3a0eb0
Chris Flook <chris.flook@tesglobal.com>
2018-10-01 14:27:47 +0100

    Update sagas test

--- client/src/sagas/tests/deployments.test.js
+++ client/src/sagas/tests/deployments.test.js
@@ -1,10 +1,11 @@
-import { put, call } from 'redux-saga/effects';
+import { put, call, select } from 'redux-saga/effects';
 import {
   fetchDeploymentsDataSaga,
 } from '../deployments';
 
 import {
   fetchDeploymentsPagination,
+  selectTableFilters,
   FETCH_DEPLOYMENTS_REQUEST,
   FETCH_DEPLOYMENTS_SUCCESS,
   FETCH_DEPLOYMENTS_ERROR,
@@ -19,7 +20,8 @@ describe('Deployments sagas', () => {
     const deploymentsData = { limit: 50, offset: 0, count: 3, items: [1, 2, 3] };
 
     const gen = fetchDeploymentsDataSaga(fetchDeploymentsPagination());
-    expect(gen.next().value).toMatchObject(put(FETCH_DEPLOYMENTS_REQUEST()));
+    expect(gen.next().value).toMatchObject(select(selectTableFilters));
+    expect(gen.next({}).value).toMatchObject(put(FETCH_DEPLOYMENTS_REQUEST()));
     expect(gen.next().value).toMatchObject(call(getDeployments, { limit: 50, offset: 0 }));
     expect(gen.next(deploymentsData).value).toMatchObject(put(FETCH_DEPLOYMENTS_SUCCESS({ data: deploymentsData } )));
     expect(gen.next().done).toBe(true);
@@ -28,7 +30,8 @@ describe('Deployments sagas', () => {
   it('should tolerate errors fetching deployments', () => {
     const error = new Error('ouch');
     const gen = fetchDeploymentsDataSaga(fetchDeploymentsPagination({ quiet: true }));
-    expect(gen.next().value).toMatchObject(put(FETCH_DEPLOYMENTS_REQUEST()));
+    expect(gen.next().value).toMatchObject(select(selectTableFilters));
+    expect(gen.next({}).value).toMatchObject(put(FETCH_DEPLOYMENTS_REQUEST()));
     expect(gen.next().value).toMatchObject(call(getDeployments, { limit: 50, offset: 0 }));
     expect(gen.throw(error).value).toMatchObject(put(FETCH_DEPLOYMENTS_ERROR({ error: error.message })));
     expect(gen.next().done).toBe(true);
@@ -38,7 +41,8 @@ describe('Deployments sagas', () => {
     const deploymentsData = { limit: 50, offset: 50, count: 3, items: [1, 2, 3] };
 
     const gen = fetchDeploymentsDataSaga(fetchDeploymentsPagination({ page: 2 }));
-    expect(gen.next().value).toMatchObject(put(FETCH_DEPLOYMENTS_REQUEST()));
+    expect(gen.next().value).toMatchObject(select(selectTableFilters));
+    expect(gen.next({}).value).toMatchObject(put(FETCH_DEPLOYMENTS_REQUEST()));
     expect(gen.next().value).toMatchObject(call(getDeployments, { limit: 50, offset: 50 }));
     expect(gen.next(deploymentsData).value).toMatchObject(put(FETCH_DEPLOYMENTS_SUCCESS({ data: deploymentsData } )));
     expect(gen.next().done).toBe(true);
