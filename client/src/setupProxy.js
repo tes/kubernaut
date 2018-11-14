6c72b6582734fe0e0cf8857854f37c7f7e7f6129
Chris Flook <chris.flook@tesglobal.com>
2018-11-14 09:19:41 +0000

    react-scripts -> v2

---
+++ client/src/setupProxy.js
@@ -0,0 +1,7 @@
+const proxy = require('http-proxy-middleware');
+
+module.exports = (app) => {
+  app.use(proxy('/api', { target: 'http://localhost:3001/' }));
+  app.use(proxy('/auth/github', { target: 'http://localhost:3001/' }));
+  app.use(proxy('/login', { target: 'http://localhost:3001/' }));
+};
