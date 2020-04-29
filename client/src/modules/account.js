4f665d970105f39509802a90f7e4c281f19b673c
Chris Flook <chris.flook@tesglobal.com>
2020-04-29 16:27:19 +0100

    well that was stupid

--- client/src/modules/account.js
+++ client/src/modules/account.js
@@ -42,7 +42,7 @@ export default handleActions({
     ...state,
     permissions: {
       ...state.permissions,
-      [payload.permission]: [payload.answer],
+      [payload.permission]: payload.answer,
     }
   }),
 }, defaultState);
