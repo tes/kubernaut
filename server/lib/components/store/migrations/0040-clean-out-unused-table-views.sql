088c6e07cd13667173f08e0ccff1b0a4eec35abf
Chris Flook <chris.flook@tesglobal.com>
2020-06-05 11:54:44 +0100

    Improve compatability

--- server/lib/components/store/migrations/0040-clean-out-unused-table-views.sql
+++ server/lib/components/store/migrations/0040-clean-out-unused-table-views.sql
@@ -1,7 +1,7 @@
 START TRANSACTION;
 
-DROP FUNCTION ensure_account_role_on_namespace;
-DROP FUNCTION ensure_account_role_on_registry;
+DROP FUNCTION ensure_account_role_on_namespace(uuid, uuid, uuid, timestamp with time zone, uuid);
+DROP FUNCTION ensure_account_role_on_registry(uuid, uuid, uuid, timestamp with time zone, uuid);
 
 DROP VIEW active_account_role__vw;
 DROP TABLE account_role_namespace;
